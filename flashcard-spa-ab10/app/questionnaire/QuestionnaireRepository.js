1074aa842a4c2a1f780ad29caa1da2931d00c7f3
nitesis <viviane.bendjus@students.fhnw.ch>
2015-11-18 11:04:35 +0100

    added QuestRepoFactory AB11.2

--- flashcard-spa-ab10/app/questionnaire/QuestionnaireRepository.js
+++ flashcard-spa-ab10/app/questionnaire/QuestionnaireRepository.js
@@ -2,27 +2,73 @@
  * 
  */
 app.service('QuestRepoService', function() {
-	
 
-	var questionnaires = [
-	{id:1, title: 'Q1', description: 'Lorem ipsum...'}, 
-	{id:2, title: 'Q2', description: 'Lorem ipsum...'}, 
-	{id:3, title: 'Q3', description: 'Lorem ipsum...'}
-	
+	var questionnaires = [ {
+		id : 1,
+		title : 'Q1',
+		description : 'Lorem ipsum...'
+	}, {
+		id : 2,
+		title : 'Q2',
+		description : 'Lorem ipsum...'
+	}, {
+		id : 3,
+		title : 'Q3',
+		description : 'Lorem ipsum...'
+	}
+
 	];
 
-	this.getAll = function () {
-		
+	this.getAll = function() {
+
 		return questionnaires;
 	};
-	
-	this.add = function(newQuestTitle){
-		var newQuestionnaire = {id: questionnaires.length + 1, title: newQuestTitle, description : 'Lorem...'};
+
+	this.add = function(newQuestTitle) {
+		var newQuestionnaire = {
+			id : questionnaires.length + 1,
+			title : newQuestTitle,
+			description : 'Lorem...'
+		};
 		questionnaires.push(newQuestionnaire);
 	};
 
 });
 
-app.factory() {
+app.factory('QuestRepoFactory', function() {
+	var questionnaires = [ {
+		id : 1,
+		title : 'Q1',
+		description : 'Lorem ipsum...'
+	}, {
+		id : 2,
+		title : 'Q2',
+		description : 'Lorem ipsum...'
+	}, {
+		id : 3,
+		title : 'Q3',
+		description : 'Lorem ipsum...'
+	}
+
+	];
+
+	function _getAll() {
+
+		return questionnaires;
+	};
+
+	function _add(newQuestTitle) {
+		var newQuestionnaire = {
+			id : questionnaires.length + 1,
+			title : newQuestTitle,
+			description : 'Lorem...'
+		};
+		questionnaires.push(newQuestionnaire);
+	};
 	
+	return {
+		add : _add,
+		getAll : _getAll
+	}
+
 });
