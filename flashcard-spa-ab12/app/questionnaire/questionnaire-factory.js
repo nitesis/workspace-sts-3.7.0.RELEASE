56890c6a2be2b68cec99420220bce8eb59cc1382
nitesis <viviane.bendjus@students.fhnw.ch>
2015-12-16 10:03:04 +0100

    added comment AB 14

--- flashcard-spa-ab12/app/questionnaire/questionnaire-factory.js
+++ flashcard-spa-ab12/app/questionnaire/questionnaire-factory.js
@@ -2,33 +2,36 @@
 
 // Alternative implementation. No changes on the client-side are needed!
 // Create the factory as Closure!
-angular.module('flashcard').factory('QuestionnaireRepository', function() {
+// vor AB 14.2 :angular.module('flashcard').factory('QuestionnaireRepository', function() {
+angular.module('flashcard').factory('QuestionnaireNetworkService', function() {
 	// Keep 'questionnaires' hidden to the rest of the world.
-	var questionnaires = [
-		 {id:1, title: 'Q1', description: 'Lorem ipsum...'},
-		 {id:2, title: 'Q2', description: 'Lorem ipsum...'},
-		 {id:3, title: 'Q3', description: 'Lorem ipsum...'}
-	];
-	
-	var index = questionnaires.length;
-	
-	function findById(id) {
-		var index = -1;
-//		for (var i=0; i < questionnaires.length; i++) {
-//			var questionnaire = questionnaires[i];
+
+//	Das hier kann jetzt weg, da mit $http mit Server kommuniziert werden soll
+//	var questionnaires = [
+//		 {id:1, title: 'Q1', description: 'Lorem ipsum...'},
+//		 {id:2, title: 'Q2', description: 'Lorem ipsum...'},
+//		 {id:3, title: 'Q3', description: 'Lorem ipsum...'}
+//	];
+//	
+//	var index = questionnaires.length;
+//	
+//	function findById(id) {
+//		var index = -1;
+////		for (var i=0; i < questionnaires.length; i++) {
+////			var questionnaire = questionnaires[i];
+////			if (questionnaire.id === id) {
+////				index = i;
+////			}
+////		}
+//		questionnaires.forEach(function(questionnaire, i) {
 //			if (questionnaire.id === id) {
 //				index = i;
-//			}
-//		}
-		questionnaires.forEach(function(questionnaire, i) {
-			if (questionnaire.id === id) {
-				index = i;
-				return;
-			};
-		});
-		return index;
-	};
-	
+//				return;
+//			};
+//		});
+//		return index;
+//	};
+//	
 	/*
 	 * Return a function object with public methods. 
 	 * Only these methods have access to the instance variables.
@@ -36,7 +39,8 @@ angular.module('flashcard').factory('QuestionnaireRepository', function() {
 	 */
 	return {
 		getAll : function() {
-			return questionnaires;
+			//return questionnaires;
+			return $http.get("/flashcard-server/questionnaires");
 		},
 		add : function(mytitle) {
 			var questionnaire = {id:++index, title: mytitle, description: 'Lorem ipsum...'};
