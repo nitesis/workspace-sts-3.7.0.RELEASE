cec2e9b59870363595ab41cc77878a85bdb2e684
nitesis <viviane.bendjus@students.fhnw.ch>
2016-01-30 17:05:56 +0100

    AngularJS init and add() Questionnaire

---
+++ flashcard-spa-ab10/app/app.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+angular.module('flashcard', []);
+
+angular.module('flashcard').controller('QuestionnaireController', function($scope){
+		
+		$scope.mytitle ='Huhu...!';
+		
+		var questionnaires = [
+		             {id:1, title: 'Q1', description: 'Lorem ipsum...'},
+		             {id:2, title: 'Q2', description: 'Lorem ipsum...'},
+		             {id:3, title: 'Q3', description: 'Lorem ipsum...'}
+		];
+		
+		//Das macht globalen Zugriff möglich
+		$scope.quest = questionnaires;	
+		
+		$scope.add = function() {
+			var questionnaire = {id:questionnaires.length+1, title: $scope.mytitle, description: 'Lorem ipsum...'};
+			questionnaires.push(questionnaire);
+			$scope.mytitle = '';
+		};
+});
