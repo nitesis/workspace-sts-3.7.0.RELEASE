c3665cd2a049c86c2bf54a8df02af48ebe4ce8e4
nitesis <viviane.bendjus@students.fhnw.ch>
2016-01-31 19:18:26 +0100

    added function for open Modal

--- flashcard-spa-ab10/app/questionnaire/questionnaire-controller.js
+++ flashcard-spa-ab10/app/questionnaire/questionnaire-controller.js
@@ -16,4 +16,19 @@ angular.module('flashcard').controller('QuestionnaireController', ['Questionnair
 		this.remove = function(id) {
 			QuestionnaireRepository.remove(id);
 		};
+		
+		// put function openModal() into the controller as an alternative to add()
+		this.openModal = function() {
+			var modalInstance = $uibModal.open({
+			      templateUrl: 'questionnaire/overlays/createDialog.html',
+			      controller: 'QuestionnaireDetailDialogController as dialog'	    	  
+			    });
+
+			modalInstance.result.then(function (questionnaire) {
+				QuestionnaireRepository.save(questionnaire);
+			}, function (error) {		
+				// something went wrong!;
+			});		
+		};	
 }]);
+
